6780d1c227a593b202deca3e3bfc0152aee774ed
ZhengYinan-AIR <83967999+ZhengYinan-AIR@users.noreply.github.com>
2023-05-28 03:13:06 +0800

    Update run_sac.py

--- run_sac.py
+++ run_sac.py
@@ -1,12 +1,16 @@
+import os
 import gym
 import torch
+import argparse
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import copy
-from torch.utils.tensorboard import SummaryWriter
 from torch.distributions import Normal
+import utils
+from utils import boolean
 
+import wandb
 
 class Actor(nn.Module):
     def __init__(self, state_dim, action_dim, hidden_width, max_action):
@@ -68,8 +72,9 @@ class Critic(nn.Module):  # According to (s,a), directly calculate Q(s,a)
 
 
 class ReplayBuffer(object):
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, state_dim, action_dim, device):
         self.max_size = int(1e6)
+        self.device = device
         self.count = 0
         self.size = 0
         self.s = np.zeros((self.max_size, state_dim))
@@ -89,47 +94,50 @@ class ReplayBuffer(object):
 
     def sample(self, batch_size):
         index = np.random.choice(self.size, size=batch_size)  # Randomly sampling
-        batch_s = torch.tensor(self.s[index], dtype=torch.float)
-        batch_a = torch.tensor(self.a[index], dtype=torch.float)
-        batch_r = torch.tensor(self.r[index], dtype=torch.float)
-        batch_s_ = torch.tensor(self.s_[index], dtype=torch.float)
-        batch_dw = torch.tensor(self.dw[index], dtype=torch.float)
+        batch_s = torch.tensor(self.s[index], dtype=torch.float).to(self.device)
+        batch_a = torch.tensor(self.a[index], dtype=torch.float).to(self.device)
+        batch_r = torch.tensor(self.r[index], dtype=torch.float).to(self.device)
+        batch_s_ = torch.tensor(self.s_[index], dtype=torch.float).to(self.device)
+        batch_dw = torch.tensor(self.dw[index], dtype=torch.float).to(self.device)
 
         return batch_s, batch_a, batch_r, batch_s_, batch_dw
 
 
 class SAC(object):
-    def __init__(self, state_dim, action_dim, max_action):
+    def __init__(self, state_dim, action_dim, max_action, config):
         self.max_action = max_action
-        self.hidden_width = 256  # The number of neurons in hidden layers of the neural network
-        self.batch_size = 256  # batch size
-        self.GAMMA = 0.99  # discount factor
-        self.TAU = 0.005  # Softly update the target network
-        self.lr = 3e-4  # learning rate
-        self.adaptive_alpha = True  # Whether to automatically learn the temperature alpha
+        self.hidden_width = config['hidden_sizes']  # The number of neurons in hidden layers of the neural network
+        self.device = config['device']
+        self.batch_size = config['batch_size']  # batch size
+        self.GAMMA = config['gamma']  # discount factor
+        self.TAU = config['tau']  # Softly update the target network
+        self.lr = config['lr']  # learning rate
+        self.actor_lr = config['actor_lr']
+        self.adaptive_alpha = config['adaptive_alpha']  # Whether to automatically learn the temperature alpha
         if self.adaptive_alpha:
             # Target Entropy = −dim(A) (e.g. , -6 for HalfCheetah-v2) as given in the paper
             self.target_entropy = -action_dim
             # We learn log_alpha instead of alpha to ensure that alpha=exp(log_alpha)>0
-            self.log_alpha = torch.zeros(1, requires_grad=True)
+            self.log_alpha = torch.zeros(1, dtype=torch.float).to(self.device).requires_grad_(True)
             self.alpha = self.log_alpha.exp()
-            self.alpha_optimizer = torch.optim.Adam([self.log_alpha], lr=self.lr)
+            self.alpha_optimizer = torch.optim.Adam([self.log_alpha], lr=self.actor_lr)
         else:
             self.alpha = 0.2
 
-        self.actor = Actor(state_dim, action_dim, self.hidden_width, max_action)
-        self.critic = Critic(state_dim, action_dim, self.hidden_width)
-        self.critic_target = copy.deepcopy(self.critic)
+        self.actor = Actor(state_dim, action_dim, self.hidden_width, max_action).to(self.device)
+        self.critic = Critic(state_dim, action_dim, self.hidden_width).to(self.device)
+        self.critic_target = copy.deepcopy(self.critic).to(self.device)
 
-        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.lr)
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.actor_lr)
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=self.lr)
 
     def choose_action(self, s, deterministic=False):
-        s = torch.unsqueeze(torch.tensor(s, dtype=torch.float), 0)
+        s = torch.unsqueeze(torch.tensor(s, dtype=torch.float), 0).to(self.device)
         a, _ = self.actor(s, deterministic, False)  # When choosing actions, we do not need to compute log_pi
-        return a.data.numpy().flatten()
+        return a.cpu().data.numpy().flatten()
 
     def learn(self, relay_buffer):
+        result = {}
         batch_s, batch_a, batch_r, batch_s_, batch_dw = relay_buffer.sample(self.batch_size)  # Sample a batch
 
         with torch.no_grad():
@@ -178,6 +186,22 @@ class SAC(object):
         # Softly update target networks
         for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
             target_param.data.copy_(self.TAU * param.data + (1 - self.TAU) * target_param.data)
+        
+        result.update({
+            'critic_loss': critic_loss,
+            'actor_loss': actor_loss,
+            'alpha_loss': alpha_loss,
+            'alpha': self.log_alpha.exp().detach().mean(),
+            'current_q': current_Q1.detach().mean(),
+            'target_q': target_Q.detach().mean()
+        })
+        return result
+    
+    def save(self, filedir):
+        torch.save(self.actor.state_dict(), os.path.join(filedir, 'policy_network.pth'))
+
+    def load(self, filedir):
+        self.actor.load_state_dict(torch.load(os.path.join(filedir, 'policy_network.pth')))
 
 
 def evaluate_policy(env, agent):
@@ -196,23 +220,15 @@ def evaluate_policy(env, agent):
 
     return int(evaluate_reward / times)
 
+def run(config):
+    env_list = ['HalfCheetah-v2', 'Hopper-v2', 'Walker2d-v2']
+    env_name = config['env_name']
+    assert env_name in env_list, "Invalid Env"
 
-def reward_adapter(r, env_index):
-    if env_index == 0:  # Pendulum-v1
-        r = (r + 8) / 8
-    elif env_index == 1:  # BipedalWalker-v3
-        if r <= -100:
-            r = -1
-    return r
+    env = gym.make(env_name)
+    env_evaluate = gym.make(env_name)  # When evaluating the policy, we need to rebuild an environment
 
-
-if __name__ == '__main__':
-    env_name = ['HalfCheetah-v2', 'Hopper-v2', 'Walker2d-v2']
-    env_index = 1
-    env = gym.make(env_name[env_index])
-    env_evaluate = gym.make(env_name[env_index])  # When evaluating the policy, we need to rebuild an environment
-    number = 1
-    seed = 0
+    seed = config['seed']
     # Set random seed
     env.seed(seed)
     env.action_space.seed(seed)
@@ -225,36 +241,47 @@ if __name__ == '__main__':
     action_dim = env.action_space.shape[0]
     max_action = float(env.action_space.high[0])
     max_episode_steps = env._max_episode_steps  # Maximum number of steps per episode
-    print("env={}".format(env_name[env_index]))
+    print("env={}".format(env_name))
     print("state_dim={}".format(state_dim))
     print("action_dim={}".format(action_dim))
     print("max_action={}".format(max_action))
     print("max_episode_steps={}".format(max_episode_steps))
+    config.update({
+        'state_dim': state_dim,
+        'action_dim': action_dim,
+        'max_action': max_action,
+        'max_episode_steps': max_episode_steps
+    })
 
-    agent = SAC(state_dim, action_dim, max_action)
-    replay_buffer = ReplayBuffer(state_dim, action_dim)
-    # Build a tensorboard
-    writer = SummaryWriter(log_dir='runs/SAC/SAC_env_{}_number_{}_seed_{}'.format(env_name[env_index], number, seed))
+    exp_name = config['algo'] + '_' + env_name
+    if config['wandb'] == True:
+        wandb.init(project=exp_name, name='test_s'+str(config['seed']), reinit=True, mode='online')
 
-    max_train_steps = 3e6  # Maximum number of training steps
-    random_steps = 25e3  # Take the random actions in the beginning for the better exploration
-    evaluate_freq = 5e3  # Evaluate the policy every 'evaluate_freq' steps
+    # Prepare Logger
+    logger_kwargs = utils.setup_logger_kwargs(exp_name, config['seed'])
+
+    logger = utils.Logger(**logger_kwargs)
+    logger.save_config(config)
+
+    agent = SAC(state_dim, action_dim, max_action, config)
+    replay_buffer = ReplayBuffer(state_dim, action_dim, config['device'])
+
     evaluate_num = 0  # Record the number of evaluations
-    evaluate_rewards = []  # Record the rewards during the evaluating
     total_steps = 0  # Record the total steps during the training
+    result = {}
 
-    while total_steps < max_train_steps:
+    while total_steps < config['max_train_steps']:
         s = env.reset()
         episode_steps = 0
         done = False
         while not done:
             episode_steps += 1
-            if total_steps < random_steps:  # Take the random actions in the beginning for the better exploration
+            if total_steps < config['random_steps']:  # Take the random actions in the beginning for the better exploration
                 a = env.action_space.sample()
             else:
                 a = agent.choose_action(s)
             s_, r, done, _ = env.step(a)
-            r = reward_adapter(r, env_index)  # Adjust rewards for better performance
+
             # When dead or win or reaching the max_episode_steps, done will be Ture, we need to distinguish them;
             # dw means dead or win,there is no next state s';
             # but when reaching the max_episode_steps,there is a next state s' actually.
@@ -265,18 +292,58 @@ if __name__ == '__main__':
             replay_buffer.store(s, a, r, s_, dw)  # Store the transition
             s = s_
 
-            if total_steps >= random_steps:
-                agent.learn(replay_buffer)
+            if total_steps >= config['random_steps']:
+                result = agent.learn(replay_buffer)
 
             # Evaluate the policy every 'evaluate_freq' steps
-            if (total_steps + 1) % evaluate_freq == 0:
+            if total_steps % config['evaluate_freq'] == 0:
                 evaluate_num += 1
                 evaluate_reward = evaluate_policy(env_evaluate, agent)
-                evaluate_rewards.append(evaluate_reward)
-                print("evaluate_num:{} \t evaluate_reward:{}".format(evaluate_num, evaluate_reward))
-                writer.add_scalar('step_rewards_{}'.format(env_name[env_index]), evaluate_reward, global_step=total_steps)
-                # Save the rewards
-                if evaluate_num % 10 == 0:
-                    np.save('./data_train/SAC_env_{}_number_{}_seed_{}.npy'.format(env_name[env_index], number, seed), np.array(evaluate_rewards))
+                result.update({'ep_r': evaluate_reward})
+                for k, v in sorted(result.items()):
+                    print(f'- {k:23s}:{v:15.10f}')
+                print(f'iteration={total_steps}')
+                if config['wandb']:
+                    wandb.log(result)
+
+                # Save model
+                dir = logger.get_dir()
+                agent.save(dir)
+                print('model saved')
+
 
             total_steps += 1
+
+            #print(total_steps)
+
+    # Save model
+    dir = logger.get_dir()
+    agent.save(dir)
+    print('model saved')
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--algo', default='SAC', type=str)
+    parser.add_argument('--env_name', default='Hopper-v2', type=str)
+    parser.add_argument('--device', default='cuda:0', type=str)
+    parser.add_argument('--wandb', default=True, type=boolean)
+    parser.add_argument('--seed', default=0, type=int)
+
+    parser.add_argument('--max_train_steps', default=int(3e6), type=int)
+    parser.add_argument('--evaluate_freq', default=int(5e3), type=int)
+    parser.add_argument('--random_steps', default=int(25e3), type=int)
+
+    parser.add_argument('--gamma', default=0.99, type=float)
+    parser.add_argument('--alpha', default=5.0, type=float)
+    parser.add_argument('--tau', default=0.005, type=float)
+    parser.add_argument('--adaptive_alpha', default=True, type=boolean)
+    parser.add_argument('--hidden_sizes', default=256)
+    parser.add_argument('--batch_size', default=256, type=int)
+    parser.add_argument('--lr', default=3e-4, type=float)
+    parser.add_argument('--actor_lr', default=3e-5, type=float)
+
+    return parser
+
+if __name__ == '__main__':
+    args = get_parser().parse_args() 
+    run(vars(args))
